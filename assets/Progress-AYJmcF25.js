149458b9590751b019d42598605384e380bd3daa
NaomiNSFW <slowpoke5001@gmail.com>
2024-07-10 22:44:55 +0100

    Hopefully fix Discord login

--- assets/Progress-AYJmcF25.js
+++
@@ -1,1 +0,0 @@
-import{j as r,C as s}from"./index-7ZRY6RmA.js";function n(){return r.jsx(s,{children:r.jsx("h1",{children:"Progress"})})}export{n as default};
---
+++ assets/TermsAndConditions-Do4fnRjA.js
@@ -0,0 +1,1 @@
+import{j as n,C as r}from"./index-DPybnkbB.js";function i(){return n.jsx(r,{children:n.jsx("h1",{children:"Terms and conditions"})})}export{i as default};
